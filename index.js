6b9a68057149c0ed841e2713841f12ce1bbd936d
Grace Do <gracenhudo@gmail.com>
2019-04-06 23:31:45 -0400

    Fixed Navigation Bug

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
-
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
 }
